d7f567bfbc0d16839f7d534ef3f292d5e117662b
Debdip Mondal <69871126+Tatz21@users.noreply.github.com>
2020-12-28 21:57:22 +0530

    update

---
+++ src/config/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = 'efba877175534d7f98cfc41f14a8836c';
