b22b147c6fdd0b67c6d0552ca56ec489eb64ff5d
Debdip Mondal <69871126+Tatz21@users.noreply.github.com>
2020-12-28 21:58:22 +0530

    update

--- src/screens/TabScreen.js
+++ src/screens/TabScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Header, Content,Left, Body, Right, Title, Tab, Tabs } from 'native-base';
 import Tab1 from './tabs/tab1';
-// import Tab2 from './tabs/tab2';
+
 import Tab3 from './tabs/tab3';
 export default class TabsExample extends Component {
   render() {
