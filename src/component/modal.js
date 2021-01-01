0de50803ed25f40abafd6a746af8d318171fa0fc
Debdip Mondal <69871126+Tatz21@users.noreply.github.com>
2021-01-01 23:39:21 +0530

    share fixed

--- src/component/modal.js
+++ src/component/modal.js
@@ -3,7 +3,7 @@ import { Dimensions, Modal, Share } from 'react-native';
 import {Container, Header, Content, Body, Left, Icon, Right, Title, Button} from 'native-base';
 import WebView from 'react-native-webview';
 
-const webViewHeight = Dimensions.get('window').height - 200;
+const webViewHeight = Dimensions.get('window').height - 100;
 
 // create a component
 class ModalComponent extends Component {
